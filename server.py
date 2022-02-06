cd4f1d0dab5eb0ae039c5553365746ba18a297cb
Djem0 <djem5000@mail.ru>
2022-02-06 08:57:53 -0800

    initial Server local:8080

--- server.py
+++ server.py
@@ -2,3 +2,6 @@ from http.server import HTTPServer, CGIHTTPRequestHandler
 server_data = ("Localhost",8080)
 server = HTTPServer(server_data, CGIHTTPRequestHandler)
 server.serve_forever()
+1 333
+
+333333
