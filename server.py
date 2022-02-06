18f89428450f583fbe8bd5dc28d08be19dc14cbc
Djem0 <djem5000@mail.ru>
2022-02-06 09:16:05 -0800

    3333 3333333

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 from http.server import HTTPServer, CGIHTTPRequestHandler
-server_data = ("Localhost",8080)
+server_data = ("Localhost", 8080)
 server = HTTPServer(server_data, CGIHTTPRequestHandler)
 server.serve_forever()
 a = 333
-
-b =333333
+b = 333333
